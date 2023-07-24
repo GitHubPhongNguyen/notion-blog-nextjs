0713d3d8b6800d032071325657a2dcd52b035b06
Him98 <nguyenvanphong5998@gmail.com>
2023-07-24 21:57:06 +0700

    task-04 create form contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,5 +4,56 @@ export const metadata = {
 };
 
 export default function ContactPage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">Contact</h1>;
+  return (
+    <>
+      <h1 className="mt-12 text-center text-3xl font-bold">
+        Mong muốn được lắng nghe từ bạn, <br />
+        Liên hệ 👋
+      </h1>
+      <form
+        className="mx-auto mt-20 max-w-[720px]"
+        action="mailto:nguyenvanphong5998@gmail.com"
+        method="post"
+        encType="text/plain"
+      >
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <span className="mb-2 block text-lg font-semibold">
+              Tên của bạn
+            </span>
+            <input
+              name="name"
+              className="w-full rounded-sm bg-gray-200 px-3 py-3 text-sm text-black"
+              type="text"
+              placeholder="Nhập tên của bạn"
+            />
+          </div>
+          <div>
+            <span className="mb-2 block text-lg font-semibold">Email</span>
+            <input
+              name="mail"
+              className="w-full rounded-sm bg-gray-200 px-3 py-3 text-sm text-black"
+              type="email"
+              placeholder="Nhập tên của bạn"
+            />
+          </div>
+        </div>
+        <div className="mt-6">
+          <span className="mb-2 block text-lg font-semibold">Nội dung</span>
+          <textarea
+            name="comment"
+            className="h-40 w-full rounded-sm bg-gray-200 px-3 py-3 text-sm text-black"
+            placeholder="Nhập tên của bạn"
+          />
+        </div>
+        <div className="mt-6">
+          <input
+            type="submit"
+            value="Gửi"
+            className="text-md cursor-pointer rounded-sm bg-gray-950 px-20 py-2 font-semibold text-slate-100"
+          />
+        </div>
+      </form>
+    </>
+  );
 }
