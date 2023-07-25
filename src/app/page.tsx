d188c94efa157d6087fcb9a54af5fa0b52b53d0a
Him98 <nguyenvanphong5998@gmail.com>
2023-07-25 23:48:43 +0700

    [ADD] add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FaInstagram, FaGithub } from 'react-icons/fa';
+
 export const metadata = {
-  title: 'Welcome | Him98',
+  title: 'Trang chủ | Him98',
 };
 
 export default function HomePage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">Home</h1>;
+  return (
+    <div className="mt-15 mx-auto grid max-w-3xl grid-cols-1 gap-9 md:mt-20 md:grid-cols-2">
+      <div>
+        <h1 className="text-5xl font-bold leading-normal drop-shadow-md">
+          Front-End React Developer 👋
+        </h1>
+        <p className="mt-6 text-xl text-gray-500 dark:text-gray-400">
+          Hi. I&apos;m Him98. A Front-End React Developer in Viet Nam 📍
+        </p>
+        <ul className="mt-6 flex gap-5">
+          <li>
+            <Link href="https://www.instagram.com/him98dev">
+              <FaInstagram
+                size={28}
+                className="drop-shadow-md duration-150 ease-in-out hover:text-orange-400"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/GitHubPhongNguyen">
+              <FaGithub
+                size={28}
+                className="drop-shadow-md duration-150 ease-in-out hover:text-orange-400"
+              />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="relative h-96 w-full overflow-hidden">
+        <Image
+          src="/him98.png"
+          alt="cover"
+          fill
+          style={{ objectFit: 'contain' }}
+        />
+      </div>
+    </div>
+  );
 }
