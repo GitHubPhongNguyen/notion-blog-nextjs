43dfd6032c25cd6fc23e8e3fddbd1c3bd66d76fb
phongnv3 <phongnv3@rikkeisoft.com>
2024-04-10 20:45:27 +0700

    feat(about): update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,5 +4,18 @@ export const metadata = {
 };
 
 export default function AboutPage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">About</h1>;
+  return (
+    <h1 className="mt-12 text-center text-3xl">
+      <div className="mb-8">
+        <h3 className="mb-4 text-lg font-semibold">
+          With more than <span className="text-2xl">3+</span> years of
+          experience in web development
+        </h3>
+        <p className="mb-4">
+          Now I am working in a <strong>big bank in Vietnam</strong> with role
+          as a <strong>front-end developer</strong>.
+        </p>
+      </div>
+    </h1>
+  );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 const Footer = () => {
   return (
     <div>
-      <h3 className="mb-4 text-lg">Become a great developer.</h3>
       <div className="columns-3">
         <div>
           <div>+84 387170123</div>
